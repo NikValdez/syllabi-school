54711cdded87b8893d4e37c88d1c56ea13f55885
Nik <nerdvestor@gmail.com>
2018-12-25 12:23:30 -0800

    add reset password backend flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const { randomBytes } = require('crypto')
+const { promisify } = require('util')
 const { hasPermission } = require('../utils')
 
 const Mutations = {
@@ -115,6 +117,54 @@ const Mutations = {
   signout(parent, args, ctx, info) {
     ctx.response.clearCookie('token')
     return { message: 'See ya!' }
+  },
+  async requestReset(parent, args, ctx, info) {
+    //1. check if this is a real user
+    const user = await ctx.db.query.user({ where: { email: args.email } })
+    if (!user) {
+      throw new Error(`No such user found for email ${args.email}`)
+    }
+    //2.set a reset token and expiry on that user
+    const randomBytesPromiseified = promisify(randomBytes)
+    const resetToken = (await randomBytesPromiseified(20)).toString('hex')
+    const resetTokenExpiry = Date.now() + 3600000 //1 hour
+    const res = await ctx.db.mutation.updateUser({
+      where: { email: args.email },
+      data: { resetToken, resetTokenExpiry }
+    })
+    console.log(res)
+    return { message: 'Thanks' }
+    //3.Email them that reset token
+  },
+  async resetPassword(parent, args, ctx, info) {
+    if (args.password !== args.confirmPassword) {
+      throw new Error("Your Passwords don't match!")
+    }
+    const [user] = await ctx.db.query.users({
+      where: {
+        resetToken: args.resetToken,
+        resetTokenExpiry_gte: Date.now() - 3600000
+      }
+    })
+    if (!user) {
+      throw new Error('This token is invalid or expired!')
+    }
+    const password = await bcrypt.hash(args.password, 10)
+
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { email: user.email },
+      data: {
+        password,
+        resetToken: null,
+        resetTokenExpiry: null
+      }
+    })
+    const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET)
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 265
+    })
+    return updatedUser
   }
 }
 
