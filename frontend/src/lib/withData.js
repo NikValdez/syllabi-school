54262fb1b8ae783cfefb02e9c7f776d25fa4a6d8
Nik <nerdvestor@gmail.com>
2019-03-01 18:01:59 -0800

    prod url

--- frontend/src/lib/withData.js
+++ frontend/src/lib/withData.js
@@ -1,9 +1,9 @@
 import ApolloClient from 'apollo-boost'
-import { endpoint } from '../config'
+import { endpoint, prodEndpoint } from '../config'
 
 function createClient({ headers }) {
   return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+    uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
     request: operation => {
       operation.setContext({
         fetchOptions: {
