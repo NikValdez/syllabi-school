310d3042b0c89ecb7a823ba8bee4416a6229a2ea
Nik <nerdvestor@gmail.com>
2019-03-03 13:56:39 -0800

    prod

--- backend/src/index.js
+++ backend/src/index.js
@@ -37,7 +37,7 @@ server.start(
   {
     cors: {
       credentials: true,
-      origin: process.env.FRONTEND_URL
+      origin: 'https://syllabi-react-prod.herokuapp.com/'
     }
   },
   deets => {
