16f6f4da3880632f08db36e72b7d8b1820152813
Nik <nerdvestor@gmail.com>
2019-03-03 14:06:03 -0800

    deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,7 +36,7 @@ server.express.use(async (req, res, next) => {
 server.start(
   {
     cors: {
-      credentials: true,
+      credentials: false,
       origin: process.env.FRONTEND_URL
     }
   },
--- frontend/src/lib/withData.js
+++ frontend/src/lib/withData.js
@@ -7,7 +7,7 @@ function createClient({ headers }) {
     request: operation => {
       operation.setContext({
         fetchOptions: {
-          credentials: 'no-cors'
+          credentials: 'include'
         },
         headers
       })
