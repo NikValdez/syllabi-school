b4853f4bfe28d33efb42f5518974f5701294a77f
Nik <nerdvestor@gmail.com>
2019-03-03 14:55:30 -0800

    deploy

--- frontend/src/lib/withData.js
+++ frontend/src/lib/withData.js
@@ -4,15 +4,16 @@ import { prodEndpoint } from '../config'
 function createClient({ headers }) {
   return new ApolloClient({
     uri: process.env.NODE_ENV === 'development' ? prodEndpoint : prodEndpoint,
-    request: operation => {
-      operation.setContext({
-        fetchOptions: {
-          credentials: 'include'
-        },
-        headers
-      })
-    }
+    // request: operation => {
+    //   operation.setContext({
+    fetchOptions: {
+      // credentials: 'include'
+      mode: 'no-cors'
+    },
+    headers
   })
+  //   }
+  // })
 }
 
 export default createClient
