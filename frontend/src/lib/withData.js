730818e1bf5bfb58078d9e0372ebcdf358289b14
Nik <nerdvestor@gmail.com>
2019-03-04 13:13:28 -0800

    prod

--- frontend/src/lib/withData.js
+++ frontend/src/lib/withData.js
@@ -1,17 +1,17 @@
 import ApolloClient from 'apollo-boost'
 import { endpoint, prodEndpoint } from '../config'
 
-function createClient({ headers }) {
+function createClient() {
   return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
-    request: operation => {
-      operation.setContext({
-        fetchOptions: {
-          mode: 'no-cors'
-        },
-        headers
-      })
-    }
+    uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint
+    // request: operation => {
+    //   operation.setContext({
+    //     fetchOptions: {
+    //       credentials: 'include'
+    //     },
+    //     headers
+    //   })
+    // }
   })
 }
 
