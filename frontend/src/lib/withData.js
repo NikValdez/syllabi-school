2c239438c58fc99db321b535fc836b566b12384f
Nik <nerdvestor@gmail.com>
2019-03-04 13:29:05 -0800

    prod

--- frontend/src/lib/withData.js
+++ frontend/src/lib/withData.js
@@ -6,9 +6,10 @@ function createClient({ headers }) {
     uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
     request: operation => {
       operation.setContext({
-        // fetchOptions: {
-        //   credentials: 'include'
-        // },
+        fetchOptions: {
+          credentials: 'include',
+          origin: 'https://syllabi-client-prod.herokuapp.com/'
+        },
         headers
       })
     }
