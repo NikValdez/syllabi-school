2b7e360dab08d143c3053e1ab54eeff3d7f99772
Nik <nerdvestor@gmail.com>
2019-03-08 10:34:57 -0800

    fix build deploy

---
+++ frontend/src/components/PrivacyPolicy.js
@@ -0,0 +1,144 @@
+import React from 'react'
+
+class PrivacyPolicy extends React.Component {
+  render() {
+    return (
+      <section className="container page">
+        <h1>Privacy Policy</h1>
+        <p>
+          Your privacy is very important to Syllabi. We understand that you are
+          trusting us with your information and we work very hard to protect
+          that privilege.
+        </p>
+        <p>
+          Syllabi Learning Solutions Inc. (“SYLLABI”) is committed to protecting
+          your privacy. We have prepared this Privacy Policy to describe our
+          practices regarding the personal information that may be collected
+          from users of our websites that post this Privacy Policy, including
+          www.gosyllabi.com (“SITE”). By submitting personal information through
+          our Site, you expressly consent to the processing of your personal
+          information in Canada in accordance with this Privacy Policy. The use
+          of personal information collected through our service shall be limited
+          to the purposes described in this Privacy Policy.
+        </p>
+        <h2>IMPORTANT NOTICE FOR SCHOOL USERS:</h2>
+        <p>
+          If you are a user who was given access to the Services by the
+          organization you are affiliated with, this Privacy Policy does not
+          govern your organization’s collection, use, or disclosure of personal
+          information through the Site. We are not responsible for your
+          organization’s collection, use, or disclosure of your personal
+          information. Please contact your organization to better understand
+          your organization’s privacy practices. Syllabi will retain personal
+          information we process on behalf of organizations for as long as
+          needed to provide services and as necessary to comply with our legal
+          obligations, resolve disputes, and enforce our agreements. Syllabi
+          uses only de-identified and aggregated data for its own purposes.
+        </p>
+        <h2>DATA YOU PROVIDE TO US.</h2>
+        <p>
+          We collect information from you, such as first and last name, e-mail
+          address and password when you create an account to log in to our
+          network. We also may retain information on your behalf, such as files
+          and messages that you store using your account. If you provide us
+          feedback or contact us via e-mail, we will collect your name and
+          e-mail address, as well as any other content included in the e-mail.
+          When you participate in one of our surveys, we may collect additional
+          profile information. We also collect other types of personal
+          information and demographic information that you provide to us
+          voluntarily.
+        </p>
+        <p>
+          Syllabi does collect and may maintain statistics relating to the
+          number of visits to the website, the types of browsers used and the
+          Internet Protocol Address of the originator. This information is used
+          only for internal purposes.
+        </p>
+        <h2>USE OF YOUR DATA</h2>
+        <p>
+          Personal information you submit to us is used either to respond to
+          requests that you make, or to aid us in serving you better. Syllabi
+          uses your personal information in the following ways: to create and
+          maintain your account; to identify you as a user in our system; to
+          operate, maintain, and improve our Site; to personalize and improve
+          your experience; to send you administrative e-mail; to respond to your
+          comments or inquiries; to send you surveys, promotional communications
+          about our products and services with your permission; to protect,
+          investigate, and deter against fraudulent, unauthorized, or illegal
+          activity.
+        </p>
+        <h2>DISCLOSURE OF YOUR PERSONAL INFORMATION</h2>
+        <p>
+          We will share your personal information with third parties only in the
+          ways that are described as follows or with your express consent.
+        </p>
+        <h2>AFFILIATES.</h2>
+        <p>
+          Although we currently do not have a parent company, any subsidiaries,
+          joint ventures, or other companies under a common control
+          (collectively, “AFFILIATES”), we may in the future. We may share some
+          or all of your information with these Affiliates, in which case we
+          will require our Affiliates to honor this Privacy Policy.
+        </p>
+        <h2>THIRD PARTY SERVICE PROVIDERS.</h2>
+        <p>
+          We may share your personal information with third party service
+          providers for the sole purpose of providing you with the Services that
+          we offer you through our Site (“INTERNAL PURPOSES”). For example, we
+          may share data with service providers who host our websites or provide
+          email services on our behalf.
+        </p>
+        <h2>OTHER DISCLOSURES.</h2>
+        <p>
+          Syllabi may disclose information about you if it believes such
+          disclosure is necessary to (a) comply with laws or to respond to
+          lawful requests and legal process; or (b) protect or defend the
+          rights, safety, or property of Syllabi, users of the Services, or any
+          person including to enforce our agreements, policies, and terms of
+          use, or (c) in an emergency to protect the personal safety of any
+          person (collectively, “a”, “b” and “c”, ENFORCEMENT PURPOSES”). We may
+          also share information about you in connection with or during
+          negotiation of any merger, financing, acquisition, bankruptcy,
+          dissolution, transaction or proceeding involving sale, transfer,
+          divestiture or disclosure of all or a portion of our business or
+          assets to another company. In these circumstances, we will only share
+          information with a company that has agreed to data privacy standards
+          no less stringent than our own. In the event that information is
+          shared in this manner, notice will be posted on our Site.
+        </p>
+        <p>
+          We may also share de-identified and aggregated data with others for
+          their own uses.
+        </p>
+        <h2>SECURITY OF YOUR PERSONAL INFORMATION.</h2>
+        <p>
+          Syllabi takes reasonable steps to help protect your personal
+          information in an effort to prevent unauthorized access, use, or
+          disclosure. Despite these measures, you should know that Syllabi
+          cannot fully eliminate security risks associated with personal
+          information. No method of transmission over the Internet, or method of
+          electronic storage, is 100% secure. Therefore, we cannot guarantee its
+          absolute security. Any content you post while using the Services is at
+          your own risk.
+        </p>
+        <h2>CALENDARING APPLICATIONS.</h2>
+        <p>
+          We may allow you to connect and share information with other Calendar
+          Applications (e.g., Google Calendar, ICalendar, Outlook). By choosing
+          to use these Platforms, you allow Syllabi to share information with
+          the Application.
+        </p>
+        <p>
+          You can view the content of our Site using other Calendaring
+          Applications but cannot alter it. We do not control the privacy
+          practices of these other Calendaring Applications and this privacy
+          policy does not cover their practices, you should visit the privacy
+          policies of any Calendaring Application or sign-in service you choose
+          to use to understand their practices.
+        </p>
+      </section>
+    )
+  }
+}
+
+export default PrivacyPolicy
---
+++ frontend/src/components/TermsAndConditions.js
@@ -0,0 +1,87 @@
+import React from 'react'
+
+class TermsAndConditions extends React.Component {
+  render() {
+    return (
+      <section className="container page">
+        <h1>Terms and Conditions</h1>
+        <h2>Legal Information</h2>
+        <p>
+          This website is owned and operated by Syllabi Learning Solutions Inc.
+          By using this website, you agree to the following terms and
+          conditions.
+        </p>
+        <h2>Website Protected by Copyright</h2>
+        <p>
+          All materials (graphics, icons, HTML, javascripts, color schemes,
+          multimedia, logos, photographs, copywriting, et al) that appear on
+          this website are the property of Syllabi Learning Solutions Inc. and
+          copyrighted by same.
+        </p>
+        <p>
+          Other materials on this website not originally produced by us (some
+          audio, video, trademarks, et al) are copyrighted by their respective
+          creators/owners and appear here either by permission, example or as
+          public domain reference material.
+        </p>
+        <h2>Currency of Website</h2>
+        <p>
+          Syllabi Learning Solutions Inc. updates the information on this
+          website regularly. However, such information is not intended to be a
+          comprehensive review of all matters and developments concerning
+          Syllabi Learning Solutions and Syllabi Learning Solutions cannot
+          guarantee the accuracy, currency or completeness of the information at
+          all times and assumes no responsibility in this regard.
+        </p>
+        <h2>Exclusion of Liability</h2>
+        <p>
+          Syllabi Learning Solutions Inc. shall not be liable for any claims,
+          expenses, damages (including direct, indirect, special or
+          consequential damages), loss of profits, opportunities or information
+          arising from:
+        </p>
+        <ul>
+          <li>
+            the use of or reliance on information contained in this website;
+          </li>
+          <li>
+            any inaccuracy or omission in such information or failure to keep
+            the information current;
+          </li>
+          <li>use of any third party websites linked to this website;</li>
+          <li>
+            any Internet software used in connection with this website or
+            computer viruses or other destructive programs encountered as a
+            result of using this website; and
+          </li>
+          <li>
+            any other matter connected with this website; even if Syllabi
+            Learning Solutions Inc. is made aware of the possibility of such
+            claims, expenses, damages or losses.
+          </li>
+        </ul>
+        <h2>Not Responsible for Internet Software or Computer Viruses</h2>
+        <p>
+          Due to technical difficulties inherent in the Internet, Internet
+          software or transmission problems could produce inaccurate or
+          incomplete copies of information contained on this website. Computer
+          viruses or other destructive programs may also be inadvertently
+          downloaded from the website. Syllabi Learning Solutions Inc. shall not
+          be liable for Internet software, computer viruses or destructive
+          programs and recommends that you install appropriate anti-virus or
+          other protective software.
+        </p>
+        <h2>Not Responsible for Linked Websites</h2>
+        <p>
+          Syllabi Learning Solutions Inc. provides links to third party websites
+          for your convenience only and the inclusion of these links does not
+          imply that Syllabi Learning Solutions Inc. monitors or endorses these
+          websites. Accordingly, Syllabi Learning Solutions Inc. accepts no
+          responsibility for such websites.
+        </p>
+      </section>
+    )
+  }
+}
+
+export default TermsAndConditions
