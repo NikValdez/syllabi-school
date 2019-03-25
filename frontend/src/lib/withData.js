e90bdcc878ff41d85de172f521dbfadd6374989d
Nik <nerdvestor@gmail.com>
2019-03-25 11:45:14 -0700

    prod deploy

--- frontend/src/lib/withData.js
+++ frontend/src/lib/withData.js
@@ -7,6 +7,7 @@ function createClient({ headers }) {
     request: operation => {
       operation.setContext({
         fetchOptions: {
+          mode: 'no-cors',
           credentials: 'include'
         },
         headers
