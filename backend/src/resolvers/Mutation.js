38091d7d9be3063c22aaa930b90e8ef74f44d28b
Nik <nerdvestor@gmail.com>
2019-05-23 16:51:37 -0700

    prod fix

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -8,15 +8,15 @@ const moment = require('moment')
 
 const Mutations = {
   async createInstitution(parent, args, ctx, info) {
-    if (!ctx.request.userId) {
-      throw new Error('You must be logged in to do that!')
-    }
-    const hasPermissions = ctx.request.user.permissions.some(permission =>
-      ['ADMIN'].includes(permission)
-    )
-    if (!hasPermissions) {
-      throw new Error("You don't have permission to do that")
-    }
+    // if (!ctx.request.userId) {
+    //   throw new Error('You must be logged in to do that!')
+    // }
+    // const hasPermissions = ctx.request.user.permissions.some(permission =>
+    //   ['ADMIN'].includes(permission)
+    // )
+    // if (!hasPermissions) {
+    //   throw new Error("You don't have permission to do that")
+    // }
     const institution = await ctx.db.mutation.createInstitution(
       {
         data: {
